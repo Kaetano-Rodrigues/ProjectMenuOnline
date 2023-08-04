7561c28f986fa950367297621dfcc1752aef8980
unknown <kaetanokaco23@gmail.com>
2023-08-03 23:47:23 -0300

    add atualizacao carrinho

--- JS/app.js
+++ JS/app.js
@@ -138,6 +138,143 @@ cardapio.metodos = {
         $('.badge-total-carrinho').html(total)
     },
 
+    abrirCarrinho: (abrir) =>{  
+
+
+        if (abrir){
+            $("#modal-carrinho").removeClass('hidden');
+            cardapio.metodos.carregarCarrinho();
+        }
+        else{
+            $("#modal-carrinho").addClass('hidden');
+        }
+            
+
+    },
+
+    // altera os textos e exibe os botoes das etapas
+    carregarEtapa: (etapa) =>{
+        
+        if (etapa == 1){
+            $("#lblTituloEtapa").text('Seu Carrinho:');
+            $("#itensCarrinho").removeClass('hidden');
+            $("#localEntrega").addClass('hidden');
+            $("#resumoCarrinho").addClass('hidden');
+
+            $(".etapa").removeClass('active');
+            $(".etapa1").addClass('active');
+
+            $("#btnEtapaPedido").removeClass('hidden');
+            $("#btnEtapaEndereco").addClass('hidden');
+            $("#btnEtapaResumo").addClass('hidden');
+            $("#btnEtapaVoltar").addClass('hidden');
+
+        }
+
+        if (etapa == 2){
+            $("#lblTituloEtapa").text('Endereço de entrega:');
+            $("#itensCarrinho").addClass('hidden');
+            $("#localEntrega").removeClass('hidden');
+            $("#resumoCarrinho").addClass('hidden');
+
+            $(".etapa").removeClass('active');
+            $(".etapa1").addClass('active');
+            $(".etapa2").addClass('active');
+
+            $("#btnEtapaPedido").addClass('hidden');
+            $("#btnEtapaEndereco").removeClass('hidden');
+            $("#btnEtapaResumo").addClass('hidden');
+            $("#btnEtapaVoltar").removeClass('hidden');
+        }
+        
+        if (etapa == 3){
+            $("#lblTituloEtapa").text('Resumo do pedido:');
+            $("#itensCarrinho").addClass('hidden');
+            $("#localEntrega").addClass('hidden');
+            $("#resumoCarrinho").removeClass('hidden');
+
+            $(".etapa").removeClass('active');
+            $(".etapa1").addClass('active');
+            $(".etapa2").addClass('active');
+            $(".etapa3").addClass('active');
+
+            $("#btnEtapaPedido").addClass('hidden');
+            $("#btnEtapaEndereco").addClass('hidden');
+            $("#btnEtapaResumo").removeClass('hidden');
+            $("#btnEtapaVoltar").removeClass('hidden');
+        }
+    },
+
+    // btn voltar etapa
+    voltarEtapa: () =>{
+        let etapa = $(".etapa.active").length;
+        cardapio.metodos.carregarEtapa(etapa - 1);
+    },
+
+    // carrega lista de itens carrinho
+    carregarCarrinho: () =>{
+        cardapio.metodos.carregarEtapa(1);
+
+        if (MEU_CARRINHO.length > 0) {
+
+            $("#itensCarrinho").html('');
+
+            $.each(MEU_CARRINHO, (i, e) =>{
+               
+                let temp = cardapio.templates.itemCarrinho.replace(/\${img}/g,e.img)
+                .replace(/\${nome}/g,e.name)
+                .replace(/\${preco}/g,e.price.toFixed(2).replace('.',','))
+                .replace(/\${id}/g,e.id)
+                .replace(/\${qntd}/g,e.qntd)
+                
+
+                $("#itensCarrinho").append(temp);
+            })
+
+        }
+        else {
+          $("#itensCarrinho").html('<p class="carrinho-vazio"><i class="fa fa-shopping-bag"></i> Seu carrinho esta vazio.</p>');
+        }
+    },
+
+   diminuirQuantidadeCarrinho: (id) =>{       
+        let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+
+        if(qntdAtual > 1){
+            $("#qntd-carrinho-" + id).text(qntdAtual - 1)
+            cardapio.metodos.autualizarCarrinho(id, qntdAtual - 1)
+        }
+        else{
+         cardapio.metodos.removerItemCarrinho(id)
+        }
+   },
+
+    aumentarQuantidadeCarrinho: (id) =>{
+        let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+
+        $("#qntd-carrinho-" + id).text(qntdAtual + 1);
+
+        cardapio.metodos.autualizarCarrinho(id, qntdAtual + 1);
+    },
+
+    removerItemCarrinho: (id) => {
+        MEU_CARRINHO = $.grep(MEU_CARRINHO,(e, i) => { return e.id != id });
+        cardapio.metodos.carregarCarrinho();
+        cardapio.metodos.atualizarBadgeTotal();
+
+    },
+
+    autualizarCarrinho: (id, qntd) =>{
+      
+        let objIndex = MEU_CARRINHO.findIndex((obj => obj.id == id));
+        MEU_CARRINHO[objIndex].qntd = qntd;
+        
+        cardapio.metodos.atualizarBadgeTotal();
+    },
+
+
+
+    //mensagens
     mensagem: (texto, cor = 'red', tempo = 3500) =>{
 
         let id = Math.floor(Date.now() * Math.random()).toString();
@@ -147,10 +284,13 @@ cardapio.metodos = {
         $("#container-mensagens").append(msg);
         
         setTimeout(() =>{
-            $("#msg-" + id).removeClass('fadeInDown');
-            $("#msg-" + id).addClass('fadeOutUp');
-            $("#msg-" + id).remove();
-        }, tempo)
+            $('#msg-' + id).removeClass('fadeInDown');
+            $('#msg-' + id).addClass('fadeOutUp');
+            setTimeout(() => {
+                $('#msg-' + id).remove();
+            }, 800);
+        },tempo)
+       
     }
 }
 
@@ -179,6 +319,25 @@ cardapio.templates = {
                     <span class="btn btn-add" onclick="cardapio.metodos.adicionarAoCarrinho('\${id}')"><i class="fas fa-shopping-bag"></i></span>
                 </div>
             </div> 
+        </div>
+    `,
+
+
+    itemCarrinho: `
+        <div class="col-12 item-carrinho">
+            <div class="img-produto">
+                <img src="\${img}"/>
+            </div>
+            <div class="dados-produto">
+                <p class="title-produto"><b>\${nome}</b></p>
+                <p class="price-produto"><b>R$ \${preco}</b></p>
+            </div>
+            <div class="add-carrinho">
+                <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidadeCarrinho('\${id}')"><i class="fas fa-minus"></i></span>
+                <span class="add-numero-itens" id="qntd-carrinho-\${id}">\${qntd}</span>
+                <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidadeCarrinho('\${id}')"><i class="fas fa-plus"></i></span>
+                <span class="btn btn-remove" onclick="cardapio.metodos.removerItemCarrinho('\${id}')"><i class="fa fa-times"></i></span>
+            </div>
         </div>
     `
 
